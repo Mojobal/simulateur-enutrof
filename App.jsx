a384ee5913fdd16c4a7feaabe3c3d15e7be58c91
Mojobal <117108227+Mojobal@users.noreply.github.com>
2025-06-20 15:13:03 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Simulateur de Dégâts - Enutrof</h1>
-      <p>Interface en cours d'intégration...</p>
-    </div>
-  );
-}
