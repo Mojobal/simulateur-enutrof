b32e7d67e72a2cc6c80edf469a37415d43d56054
Mojobal <117108227+Mojobal@users.noreply.github.com>
2025-06-20 15:09:33 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Simulateur de Dégâts - Enutrof</h1>
+      <p>Interface en cours d'intégration...</p>
+    </div>
+  );
+}
